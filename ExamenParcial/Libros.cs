96fe4ebab280b2763331f611c00e2fa884eee67e
Julián Vásquez <julianvg@umes.edu.gt>
2024-05-23 08:12:45 -0600

    Agregar archivos de proyecto.

---
+++ ExamenParcial/Libros.cs
@@ -0,0 +1,21 @@
+﻿namespace ExamenParcial
+{
+    public class Libros
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string Genero { get; set; }
+        public int AnioPublicacion { get; set; }
+
+        public Libros()
+        {
+            Titulo = string.Empty;
+            Autor = string.Empty;
+            Genero = string.Empty;
+            AnioPublicacion = 0;
+
+
+        }
+
+    }
+}
