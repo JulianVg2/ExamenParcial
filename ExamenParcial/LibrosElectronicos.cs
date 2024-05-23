279711c1fe287273e8f350ceed1f0192690c3fe7
Julián Vásquez <julianvg@umes.edu.gt>
2024-05-23 08:42:14 -0600

    Inciso 1 y 2 Calificados

---
+++ ExamenParcial/LibrosElectronicos.cs
@@ -0,0 +1,14 @@
+﻿namespace ExamenParcial
+{
+    public class LibrosElectronicos : Libros
+    {
+        public string Formato { get; set; }
+        public int TamañoArchivo { get; set; }
+
+        public LibrosElectronicos()
+        {
+            Formato = string.Empty;
+            TamañoArchivo = 0;
+        }
+    }
+}
---
+++ ExamenParcial/Revistas.cs
@@ -0,0 +1,16 @@
+﻿namespace ExamenParcial
+{
+    public class Revistas : Libros
+    {
+        public int Volumen { get; set; }
+        public int NumeroEjemplar { get; set; }
+
+        public Revistas()
+        {
+            Volumen = 0;
+            NumeroEjemplar = 0;
+        }
+
+
+    }
+}
